56c1f51fb34496abbed845a76bc1c0d21b3e0bcf
ys <emost22@naver.com>
2021-01-13 17:06:55 +0900

    boj15988 1, 2, 3 더하기 3

---
+++ BOJ/src/BOJ/dp/Boj15988.java
@@ -0,0 +1,35 @@
+package BOJ.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj15988 {
+	static final int MOD = 1000000009;
+	static int N;
+	static int dp[] = new int[1000001];
+
+	static void init() {
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 4; i <= 1000000; i++) {
+			dp[i] = ((dp[i - 1] + dp[i - 2]) % MOD + dp[i - 3]) % MOD;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		init();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int tc = Integer.parseInt(st.nextToken());
+		while (tc-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			System.out.println(dp[N]);
+		}
+	}
+}
