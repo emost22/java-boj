c4993146825387601d2ed255685a815a36763643
ys <emost22@naver.com>
2021-01-29 21:37:10 +0900

    boj11399 ATM

---
+++ BOJ/src/BOJ/greedy/Boj11399.java
@@ -0,0 +1,40 @@
+package BOJ.greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj11399 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int list[], dp[];
+	static int N, ans;
+
+	static void solve() {
+		for (int i = 1; i <= N; i++) {
+			dp[i] = dp[i - 1] + list[i];
+			ans += dp[i];
+		}
+
+		System.out.println(ans);
+	}
+
+	static void input() throws Exception {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		list = new int[N + 1];
+		dp = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(list);
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+		solve();
+	}
+}
