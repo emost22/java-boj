075a0b775ffc878245a60595360e173c5b4ef7fe
ys <emost22@naver.com>
2021-01-31 21:54:49 +0900

    boj15740 A+B - 9

---
+++ BOJ/src/BOJ/수학/Boj15740.java
@@ -0,0 +1,24 @@
+package BOJ.수학;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Boj15740 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static BigInteger A, B;
+	
+	static void input() throws Exception{
+		st = new StringTokenizer(br.readLine());
+		
+		A = new BigInteger(st.nextToken());
+		B = new BigInteger(st.nextToken());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		input();
+		System.out.println(A.add(B));
+	}
+}
