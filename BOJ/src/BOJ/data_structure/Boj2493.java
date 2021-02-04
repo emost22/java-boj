68c1754aee2901396d1dabf4be0761ddeb460fd6
ys <emost22@naver.com>
2021-02-04 22:36:58 +0900

    boj2493 탑

--- BOJ/src/BOJ/data_structure/Boj2493.java
+++ BOJ/src/BOJ/data_structure/Boj2493.java
@@ -1,7 +1,6 @@
 package BOJ.data_structure;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
@@ -11,33 +10,30 @@ public class Boj2493 {
 	static StringTokenizer st;
 	static StringBuffer sb = new StringBuffer();
 	static Stack<Integer> s = new Stack<>();
-	static int list[], ans[];
+	static int list[];
 	static int N;
 
-	static void print() {
+	static void func() {
 		for (int i = 0; i < N; i++) {
-			sb.append(ans[i] + " ");
+			while (!s.isEmpty() && list[s.peek()] < list[i])
+				s.pop();
+
+			if (s.isEmpty()) {
+				s.push(i);
+				sb.append("0 ");
+			} else {
+				sb.append(s.peek() + 1 + " ");
+				s.push(i);
+			}
 		}
 
 		System.out.println(sb.toString());
 	}
 
-	static void func() {
-		for (int i = N - 1; i >= 0; i--) {
-			while (!s.isEmpty() && list[s.peek()] < list[i]) {
-				ans[s.peek()] = i + 1;
-				s.pop();
-			}
-
-			s.push(i);
-		}
-	}
-
-	static void input() throws IOException {
+	static void input() throws Exception {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		list = new int[N];
-		ans = new int[N];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
@@ -45,9 +41,8 @@ public class Boj2493 {
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		input();
 		func();
-		print();
 	}
 }
