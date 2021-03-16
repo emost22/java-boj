486d7351b667c8eb2be4156fbe66579b578daf95
ys <emost22@naver.com>
2021-03-16 17:28:31 +0900

    boj1759 암호 만들기

--- BOJ/src/BOJ/dfs/Boj1759.java
+++ BOJ/src/BOJ/dfs/Boj1759.java
@@ -2,51 +2,39 @@ package BOJ.dfs;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj1759 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
+	static StringBuffer sb = new StringBuffer();
+	static char list[] = new char[16];
 	static int L, C, a, b;
-	static char ch[];
-	static boolean visit[];
-	static ArrayList<char[]> list = new ArrayList<>();
 
-	static void dfs(int idx, int cnt) {
+	static void func(int idx, int cnt, String str) {
 		if (cnt == L) {
 			if (a < 1 || b < 2)
 				return;
-			for (int i = 0; i < list.size(); i++) {
-				System.out.print(list.get(i)[0]);
-			}
-			System.out.println();
+			sb.append(str).append("\n");
 			return;
 		}
+
 		if (idx >= C)
 			return;
 
 		for (int i = idx; i < C; i++) {
-			if (visit[i])
-				continue;
-
-			if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'i')
+			if (list[i] == 'a' || list[i] == 'e' || list[i] == 'i' || list[i] == 'o' || list[i] == 'u')
 				a++;
 			else
 				b++;
 
-			visit[i] = true;
-			list.add(new char[] { ch[i] });
-			dfs(i + 1, cnt + 1);
-			list.remove(list.size() - 1);
-			visit[i] = false;
+			func(i + 1, cnt + 1, str + list[i]);
 
-			if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'i')
+			if (list[i] == 'a' || list[i] == 'e' || list[i] == 'i' || list[i] == 'o' || list[i] == 'u')
 				a--;
 			else
 				b--;
-
 		}
 	}
 
@@ -55,19 +43,16 @@ public class Boj1759 {
 		L = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
-		ch = new char[C];
-		visit = new boolean[C];
-
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < C; i++) {
-			ch[i] = st.nextToken().charAt(0);
+			list[i] = st.nextToken().charAt(0);
 		}
-
-		Arrays.sort(ch);
+		Arrays.sort(list, 0, C);
 	}
 
 	public static void main(String[] args) throws Exception {
 		input();
-		dfs(0, 0);
+		func(0, 0, "");
+		System.out.print(sb.toString());
 	}
 }
