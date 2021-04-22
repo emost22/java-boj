cc678fd295f7ce24bdd3d12c1fc0047b120ce4d4
ys <emost22@naver.com>
2021-04-22 21:58:02 +0900

    boj20207 달력

---
+++ BOJ/src/BOJ/구현/Boj20207.java
@@ -0,0 +1,49 @@
+package BOJ.구현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj20207 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int list[] = new int[366];
+	static int N, ans;
+
+	static void func() {
+		int max = 0;
+		int con = 0;
+		for (int i = 1; i <= 365; i++) {
+			if (list[i] > 0) {
+				con++;
+				max = Math.max(max, list[i]);
+			} else {
+				ans += (con * max);
+				con = 0;
+				max = 0;
+			}
+		}
+
+		ans += (con * max);
+		System.out.println(ans);
+	}
+
+	static void input() throws Exception {
+		int l, r;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			l = Integer.parseInt(st.nextToken());
+			r = Integer.parseInt(st.nextToken());
+
+			for (int j = l; j <= r; j++)
+				list[j]++;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+		func();
+	}
+}
